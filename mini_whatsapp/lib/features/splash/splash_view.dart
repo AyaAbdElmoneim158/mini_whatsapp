b7c9379a579f30eab87243abbe58e088a846fe01
AyaAbdElmoneim158 <ayaabdelmon@gmail.com>
2024-10-30 08:33:32 +0300

    Add Splash screen ui

--- mini_whatsapp/lib/features/splash/splash_view.dart
+++ mini_whatsapp/lib/features/splash/splash_view.dart
@@ -1,80 +1,44 @@
-import 'dart:async';
+// ignore_for_file: use_build_context_synchronously
 import 'package:flutter/material.dart';
-import '../../core/utils/constants/colors.dart';
+import '../../../../core/common/full_scroll_screen_container.dart';
 import '../../core/utils/constants/images.dart';
-import '../../core/utils/constants/styles.dart';
-import '../../core/utils/constants/spacing.dart';
-
+import '../../core/utils/function_helper.dart';
 
 class SplashView extends StatefulWidget {
   const SplashView({super.key});
 
   @override
-  // ignore: library_private_types_in_public_api
-  _SplashViewState createState() => _SplashViewState();
+  State<SplashView> createState() => _SplashViewState();
 }
 
 class _SplashViewState extends State<SplashView> {
   @override
   void initState() {
-    Timer(const Duration(seconds: 2), () {
-      if (mounted) {
-        //   Navigator.pushAndRemoveUntil(
-        //   context,
-        //   MaterialPageRoute(
-        //     builder: (_) => const WelcomePage(),
-        //   ),
-        //       (route) => false,
-        // );
-      }
-    });
     super.initState();
+    // _navigateToLogin(context);
+  }
+
+  void _navigateToLogin(BuildContext context) {
+    Future.delayed(const Duration(seconds: 3), () {
+      Navigator.of(context).pushReplacementNamed('/login');
+    });
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        backgroundColor: AppColors.backgroundLight,
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Container(),
-            Image.asset(
-              AppAssets.whatsAppLogo,
-              color: Colors.white,
-              width: 100,
-              height: 100,
-            ),
-            Column(
-              children: [
-                Text(
-                  "from",
-                  style: AppStyles.font18GrayRegular
-                ),
-                                verticalSpace(10),
+    return FullScrollScreenContainer(
+      hasHight: true,
+      body: _buildSplashContent(),
+    );
+  }
 
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Image.asset(
-                      AppAssets.meta,
-                      color: Colors.white,
-                      width: 35,
-                      height: 35,
-                    ),
-                  
-                    horizontalSpace(5),
-                    const Text(
-                      "Meta",
-                      style:
-                          TextStyle(fontSize: 25, fontWeight: FontWeight.w500),
-                    ),
-                  ],
-                ),
-                verticalSpace(30)
-              ],
-            )
-          ],
-        ));
+  Widget _buildSplashContent() {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        Image.asset(AppAssets.whatsAppLogo, height: 100),
+        HelperFunctions.showLoader(),
+      ],
+    );
   }
 }
--- mini_whatsapp/lib/mini_whatsapp.dart
+++ mini_whatsapp/lib/mini_whatsapp.dart
@@ -1,9 +1,8 @@
 import 'package:flutter/material.dart';
 
-import 'core/routes/router.dart';
-import 'core/routes/routes.dart';
-import 'core/utils/constants/images.dart';
+import 'core/theme/theme.dart';
 import 'core/utils/constants/strings.dart';
+import 'features/splash/splash_view.dart';
 
 class MiniWhatsapp extends StatelessWidget {
   const MiniWhatsapp({super.key});
@@ -11,48 +10,18 @@ class MiniWhatsapp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: AppStrings.appTitle,
-      theme: ThemeData(),
       debugShowCheckedModeBanner: false,
-
-      //! Route ----------------------------------------------------------------------
-      onGenerateRoute: AppRouter.onGenerate,
-      initialRoute: AppRoutes.initRoute,
-      onUnknownRoute: (settings) => MaterialPageRoute(
-        builder: (_) => const NotFoundPage(),
-      ),
+      title: AppStrings.appName,
+      themeMode: ThemeMode.light,
+      theme: AppTheme.lightTheme,
+      initialRoute: '/',
+      routes: _buildRoutes(),
     );
   }
-}
 
-class NotFoundPage extends StatelessWidget {
-  const NotFoundPage({
-    super.key,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Image.asset(
-              AppAssets.whatsappBgImage,
-              width: 250,
-            ),
-            const SizedBox(height: 12),
-            Text(
-              'Sorry, this page was not found',
-              style: Theme.of(context).textTheme.titleMedium!.copyWith(
-                    color: const Color(0xFF83839c),
-                    fontWeight: FontWeight.w600,
-                  ),
-            ),
-          ],
-        ),
-      ),
-    );
+  Map<String, WidgetBuilder> _buildRoutes() {
+    return {
+      '/': (context) => const SplashView(),
+    };
   }
 }
